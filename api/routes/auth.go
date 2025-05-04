a4953c0e0edd34d582896c916d05746ddf0e34e5
Mihail <Misha200417@gmail.com>
2025-05-05 01:57:13 +0300

    initialization AuthRepository and AuthService in auth routes

--- api/routes/auth.go
+++ api/routes/auth.go
@@ -3,12 +3,18 @@ package routes
 import (
 	"github.com/Memchikkr/go-routes/api/controllers"
 	"github.com/Memchikkr/go-routes/bootstrap"
+	"github.com/Memchikkr/go-routes/internal/repositories"
+	"github.com/Memchikkr/go-routes/internal/services"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
-func NewAuthRouter(env *bootstrap.Env, group *gin.RouterGroup) {
+func NewAuthRouter(env *bootstrap.Env, db *sqlx.DB, group *gin.RouterGroup) {
+	ar := repositories.NewAuthRepository(db)
 	ac := &controllers.AuthController{
+		Service: services.NewAuthService(ar, env),
 		Env: env,
 	}
 	group.POST("/auth", ac.Auth)
+	group.POST("/auth/refresh", ac.Refresh)
 }
