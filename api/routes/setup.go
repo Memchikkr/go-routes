d26bbbdae57b15fea98ef5225df081d2ae89da36
Mihail <Misha200417@gmail.com>
2025-06-03 02:23:48 +0300

    setup all new routes with protected router

--- api/routes/setup.go
+++ api/routes/setup.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/Memchikkr/go-routes/api/middlewares"
 	"github.com/Memchikkr/go-routes/bootstrap"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
@@ -9,4 +10,12 @@ import (
 func Setup(env *bootstrap.Env, db *sqlx.DB, gin *gin.Engine) {
 	publicRouter := gin.Group("")
 	NewAuthRouter(env, db, publicRouter)
+	protectedRouter := gin.Group("")
+	// Middleware to verify AccessToken
+	protectedRouter.Use(middlewares.JwtAuthMiddleware(env.AccessTokenSecret))
+	NewUserRouter(env, db, protectedRouter)
+	NewTripRouter(env, db, protectedRouter)
+	NewBookingRouter(env, db, protectedRouter)
+	NewVehicleRouter(env, db, protectedRouter)
+	NewRouteRouter(env, db, protectedRouter)
 }
